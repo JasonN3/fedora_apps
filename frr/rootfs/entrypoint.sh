49abf62bf7fbea26471de62a831ff569110a09eb
Jason N. <33561705+JasonN3@users.noreply.github.com>
2025-06-25 14:39:58 -0400

    fixes

--- frr/rootfs/entrypoint.sh
+++ frr/rootfs/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i 's/^#?frr_global_options=.*/frr_global_options="-f /etc/frr/frr.${ROUTER}.conf
+sed -i 's/^#?frr_global_options=.*/frr_global_options="-f /etc/frr/frr.${ROUTER}.conf"' /etc/frr/daemons
 
-exec /usr/lib/frr/docker-start $@
+exec /usr/lib/frr/docker-start "$@"
