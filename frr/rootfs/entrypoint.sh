f072f0e0953f958effd33bcc502281311af7517c
Jason N. <33561705+JasonN3@users.noreply.github.com>
2025-06-26 13:51:02 -0400

    configure daemons

--- frr/rootfs/entrypoint.sh
+++ frr/rootfs/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i \
-  "s;^#\?frr_global_options=.*;frr_global_options=\"-f /etc/frr/frr.${ROUTER}.conf\";" \
-  /etc/frr/daemons
+
+ln -sf /etc/frr/frr.${ROUTER}.conf /etc/frr/frr.conf
+ln -sf /etc/frr/daemons.${ROUTER} /etc/frr/daemons
 
 exec /usr/lib/frr/docker-start "$@"
