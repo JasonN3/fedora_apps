345e4e7a9a72d4a5c1dbaa52d05f488c445508bf
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2025-07-18 10:46:20 -0400

    manage processes

--- openvpn/rootfs/entrypoint.sh
+++ openvpn/rootfs/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
+set -euo pipefail
 
+# start auth helper
 /usr/local/bin/openvpn-auth-oauth2 --config "/etc/openvpn/openvpn-auth-oauth2.yaml" &
+oauth_pid=$!
 
-exec "$@"
+# forward SIGTERM/SIGINT to both processes and reap properly
+term_handler() {
+  kill -TERM "$oauth_pid" 2>/dev/null || true
+  kill -TERM "$openvpn_pid" 2>/dev/null || true
+  wait "$oauth_pid" "$openvpn_pid"
+}
+trap term_handler INT TERM
+
+# start OpenVPN in foreground
+"$@" &
+openvpn_pid=$!
+
+# wait on foreground process(es)
+wait "$openvpn_pid"
