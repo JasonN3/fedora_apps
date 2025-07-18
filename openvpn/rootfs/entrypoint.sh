1d24425c6e27a9cdf27538ff72fef7a56e7da5dd
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2025-07-18 10:58:53 -0400

    use dumb-init

--- openvpn/rootfs/entrypoint.sh
+++ openvpn/rootfs/entrypoint.sh
@@ -1,21 +1,7 @@
-#!/bin/sh
-set -euo pipefail
+#!/usr/bin/dumb-init
 
 # start auth helper
 /usr/local/bin/openvpn-auth-oauth2 --config "/etc/openvpn/openvpn-auth-oauth2.yaml" &
-oauth_pid=$!
 
-# forward SIGTERM/SIGINT to both processes and reap properly
-term_handler() {
-  kill -TERM "$oauth_pid" 2>/dev/null || true
-  kill -TERM "$openvpn_pid" 2>/dev/null || true
-  wait "$oauth_pid" "$openvpn_pid"
-}
-trap term_handler INT TERM
-
-# start OpenVPN in foreground
-"$@" &
-openvpn_pid=$!
-
-# wait on foreground process(es)
-wait "$openvpn_pid"
+# start OpenVPN
+"$@"
