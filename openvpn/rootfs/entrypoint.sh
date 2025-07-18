30e7633232958406112dd6809b63b5929cd08f7f
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2025-07-18 11:14:59 -0400

    launch openvpn before auth helper

--- openvpn/rootfs/entrypoint.sh
+++ openvpn/rootfs/entrypoint.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/dumb-init
+#!/usr/bin/dumb-init /bin/sh
+
+# start OpenVPN
+"$@" &
 
 # start auth helper
 /usr/local/bin/openvpn-auth-oauth2 --config "/etc/openvpn/openvpn-auth-oauth2.yaml" &
 
-# start OpenVPN
-"$@"
+fg %1
